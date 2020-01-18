1730972cfbbf3523b9c65c526c3a4b8d42972068
ramialkaro <almemari22ramosh@gmail.com>
2020-01-18 22:08:22 +0200

    filter component added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import GuestsForm from '../guests/GuestsForm'
 import GuestCounter from '../guests/GuestCounter'
+import GuestFilter from '../guests/GuestFilter'
 
  const Home = () => {
     return (
         <div className="app-container">
             <div className="main">
+                <div className="filter">
+                    <GuestFilter/>
+                </div>
                 <GuestsForm/>
                 <GuestCounter/>
             </div>
