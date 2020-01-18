03a517c59ab552b48a828de5420f4e7721a14230
ramialkaro <almemari22ramosh@gmail.com>
2020-01-18 21:51:23 +0200

    navbar and form added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navbar from './comonents/layouts/Navbar';
+import Home from './components/pages/Home';
 function App() {
   return (
     <div>
       <Navbar/>
-     Hello Rami 
+      <Home/>
     </div>
   );
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import GuestsForm from '../guests/GuestsForm'
+
+ const Home = () => {
+    return (
+        <div className="app-container">
+            <GuestsForm/>
+        </div>
+    )
+}
+export default Home
