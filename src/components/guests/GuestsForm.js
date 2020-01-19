e7c1029a5e3800810116305fea7493495598e60f
ramialkaro <almemari22ramosh@gmail.com>
2020-01-19 12:34:17 +0200

    data read from into console

--- src/components/guests/GuestsForm.js
+++ src/components/guests/GuestsForm.js
@@ -1,25 +1,50 @@
  
-import React from 'react'
+import React,{useState} from 'react'
 
 const GuestForm = () => {
+  const [guest, setGuest] = useState({
+    name:'',
+    phone:'',
+    dietary:'Non-Veg'
+  })
+
+  const  {name, phone, dietary} = guest
+
+  const handleChange=e=>{
+    setGuest({
+      ...guest, 
+      [e.target.name]:e.target.value
+    })
+  }
+
+  const handleSumbit=e=>{
+    e.preventDefault()
+    console.log(guest)
+    setGuest({
+      name:'',
+      phone:'',
+      dietary:'Non-Veg'
+    })
+  }
+  
   return (
     <div className="invite-section">
       <h1>Invite Someone</h1>
-      <form >
-        <input type="text" placeholder="Name" name="name" />
-        <input type="text" placeholder="Phone" name="phone" />
+      <form onSubmit={handleSumbit}>
+        <input type="text" placeholder="Name" name="name" value={name} onChange={handleChange}/>
+        <input type="text" placeholder="Phone" name="phone" value={phone} onChange={handleChange}/>
         <p className="options-label">Dietary</p>
         <div className="options">
           <label className="container">Non-veg
-        <input type="radio" name="dietary" value='Non-Veg' />
+        <input type="radio" name="dietary" value='Non-Veg' checked={dietary==='Non-Veg'} onChange={handleChange}/>
             <span className="checkmark"></span>
           </label>
           <label className="container">Vegan
-        <input type="radio" name="dietary" value='Vegan' />
+        <input type="radio" name="dietary" value='Vegan' onChange={handleChange}/>
             <span className="checkmark"></span>
           </label>
           <label className="container">Pescetarian
-        <input type="radio" name="dietary" value='Pescetarian' />
+        <input type="radio" name="dietary" value='Pescetarian' onChange={handleChange}/>
             <span className="checkmark"></span>
           </label>
         </div>
